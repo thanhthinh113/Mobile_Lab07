ce1add790025c4e0f16afc73507495d0e9fdee5d
thanhthinh113 <thanhthinhit110823@gmail.com>
2024-10-09 00:07:34 +0700

    API_Screen02

--- view/API_Screen02.js
+++ view/API_Screen02.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Image,
   SafeAreaView,
@@ -7,22 +7,140 @@ import {
   View,
   TouchableOpacity,
   TextInput,
+  FlatList,
 } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import axios from "axios";
+
 export default function API_Screen01() {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://67055f04031fd46a830fb4fb.mockapi.io/users")
+      .then((response) => setUser(response.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const displayContent = ({ item }) => {
+    return (
+      <View style={styles.card}>
+        <Text style={styles.cardContent}>{item.content}</Text>
+        <AntDesign style={styles.ant} name="edit"></AntDesign>
+      </View>
+    );
+  };
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container}>
-<Text>
-    name
-</Text>
-
+        <View style={styles.header}>
+          <AntDesign style={styles.ant} name="arrowleft"></AntDesign>
+          <View style={styles.infor}>
+            <Image
+              style={styles.imgHeader}
+              source={require("../image/icon.png")}
+            />
+            <View style={styles.status}>
+              <Text style={styles.name}>HI Twinkle</Text>
+              <Text style={styles.textContent}>Have a great day ahead</Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.search}>
+          <TextInput style={styles.input} placeholder="Search" />
+        </View>
+        <View style={styles.display}>
+          <FlatList
+            data={user}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={displayContent}
+          />
+        </View>
+        <View style={styles.add}>
+          <TouchableOpacity
+            style={styles.button1}
+            onPress={() => navigation.navigate("API_Screen02")}
+          >
+            <AntDesign style={styles.plus} name="plus"></AntDesign>
+          </TouchableOpacity>
+        </View>
       </View>
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-})
+  container: {
+    flex: 1,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  header: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  ant: {
+    fontSize: 24,
+  },
+  infor: {
+    flexDirection: "row",
+  },
+  status: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: "bold",
+    paddingBottom: 3,
+  },
+  search: {
+    flex: 1,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 4,
+    borderColor: "gray",
+  },
+  display: {
+    flex: 4,
+  },
+  add: {
+    flex: 2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button1: {
+    backgroundColor: "#00BDD6",
+    padding: 15,
+    borderRadius: 50,
+    width: 80,
+    height: 80,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  plus: {
+    color: "white",
+    fontSize: 24,
+  },
+
+  card:{
+    backgroundColor: 'grey',
+    padding: 18,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius:30,
+    flexDirection:"row",
+    justifyContent:"space-between"
+  },
+  cardContent:{
+    fontWeight:"bold",
+    fontSize:18
+  },
+});
